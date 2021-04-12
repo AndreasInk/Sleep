e8f02d414ccf59efe0d66da56dd36588c8c2f3b8
Andreas Ink <andreasink17@gmail.com>
2021-04-12 10:48:12 -0400

    first commit

---
+++ Sleep/Day.swift
@@ -0,0 +1,20 @@
+//
+//  Day.swift
+//  Sleep
+//
+//  Created by Andreas on 4/11/21.
+//
+
+import SwiftUI
+
+struct Day: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Day_Previews: PreviewProvider {
+    static var previews: some View {
+        Day()
+    }
+}
---
+++ Sleep/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Sleep
+//
+//  Created by Andreas on 4/11/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
